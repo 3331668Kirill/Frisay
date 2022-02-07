445bf55772dbc7fc0a128dc97f2b7f53a297170c
karanzhenya <63317606+karanzhenya@users.noreply.github.com>
2022-02-07 21:48:01 +0300

    refactoring

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {authReducer} from "./reducers/registration-reducer";
+import {loginReducer} from "../pages/Login/login-reducer";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-    tasks: authReducer
+    tasks: authReducer,
+    login: loginReducer
 })
 // непосредственно создаём store
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
